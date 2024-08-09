87d063dc03dd419836b352788c805735e0e44f32
cade-th <cade.thorn9@gmail.com>
2024-08-09 12:10:48 -0400

    added uninstall etc

--- install.sh
+++ install.sh
@@ -1,20 +1,26 @@
-# Set up symlinks
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/nvim ~/.config/nvim
-
 # Install dependencies
 if [ "$(uname)" == "Darwin" ]; then
   # macOS
-  brew install neovim
+  brew install stow neovim git curl zsh tmux
 elif [ -n "$(command -v apt-get)" ]; then
   # Ubuntu/Debian
   sudo apt-get update
-  sudo apt-get install -y neovim git curl
+  sudo apt-get install -y stow neovim git curl tmux zsh
 elif [ -n "$(command -v pacman)" ]; then
   # Arch Linux
-  sudo pacman -Syu neovim git curl --noconfirm
+  sudo pacman -Syu stow neovim git curl tmux zsh --noconfirm
 fi
+
+# Clone LazyVim starter into nvim directory
+git clone https://github.com/LazyVim/starter.git ~/dotfiles/nvim/.config/nvim
+
+cd ~/dotfiles
+
+rm -rf /nvim/.config/nvim/.git
+
+stow tmux
+stow zsh
+stow nvim
 
 # Set up Neovim
 nvim --headless "+Lazy sync" +qa
